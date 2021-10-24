5ffe5b76a3efefe4bd54dd0aeee3699387431f19
Gilberto De Leon <deleonquintero@gmail.com>
2021-10-23 23:29:06 -0500

    Plex and Home assisntant ARM Raspberry
    Plex and Home assisntant for:
    Arm devices including Raspberry pi

---
+++ Containers Deploy/Home Assistant.sh
@@ -0,0 +1,8 @@
+ docker run  \
+--restart unless-stopped  \
+--init -d  \
+--name="home-assistant"  \
+-e "TZ=America/Bogota"  \
+-v /mnt/media/homeasistant:/config  \
+-p 8123:8123  \
+homeassistant/home-assistant:latest

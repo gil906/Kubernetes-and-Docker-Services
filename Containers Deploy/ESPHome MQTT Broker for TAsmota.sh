5a0b6630f294ab41ef31747894514793737a7c30
Gilberto De Leon <deleonquintero@gmail.com>
2021-10-24 20:09:47 -0500

    Update ESPHome MQTT Broker for TAsmota.sh

--- Containers Deploy/ESPHome MQTT Broker for TAsmota.sh
+++ Containers Deploy/ESPHome MQTT Broker for TAsmota.sh
@@ -1,3 +1,6 @@
+# To execute it directly on a Ubuntu as a bash script from github:
+# wget -O - https://github.com/gil906/Docker-on-ARM-devices/raw/main/Containers%20Deploy/ESPHome%20MQTT%20Broker%20for%20TAsmota.sh | bash
+
 docker run -d \
 --name="esphome"  \
 -p 6052:6052  \

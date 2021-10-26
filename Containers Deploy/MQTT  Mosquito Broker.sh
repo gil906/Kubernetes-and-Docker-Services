2ace486b479e84db5bd7f7e7c216c12c6a9e6a9e
Gilberto De Leon <deleonquintero@gmail.com>
2021-10-25 23:11:29 -0500

    Update MQTT  Mosquito Broker.sh

--- Containers Deploy/MQTT  Mosquito Broker.sh
+++ Containers Deploy/MQTT  Mosquito Broker.sh
@@ -1,3 +1,6 @@
+# To execute it directly on a Ubuntu as a bash script from github:
+# wget -O - https://github.com/gil906/Docker-on-ARM-devices/raw/main/Containers%20Deploy/MQTT%20%20Mosquito%20Broker.sh | bash
+
 docker run -it \
 -p 1883:1883 \
 -p 9001:9001 \

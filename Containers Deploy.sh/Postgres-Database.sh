8831962872737177e1affc973f55082f795025ec
Gilberto De Leon <deleonquintero@gmail.com>
2021-11-08 00:09:47 -0500

    Update Postgres-Database.sh

--- Containers Deploy.sh/Postgres-Database.sh
+++ Containers Deploy.sh/Postgres-Database.sh
@@ -1,4 +1,14 @@
 
+
+# Create Volume PGDATA
+# docker volume create --name=pgdata
+
+docker volume create --driver local \
+    --opt type=btrfs \
+    --opt device=:/mnt/media/postgres/data/pgdata \
+    PGDATA
+
+
 # Postgres - Working
 docker run -d \
 --name postgres \
@@ -6,12 +16,39 @@ docker run -d \
 --restart unless-stopped \
 -e POSTGRES_USER=postgres \
 -e POSTGRES_PASSWORD=postgres \
--e PGDATA=/mnt/media/postgres/data/pgdata \
+-e PGDATA=/mnt/media/postgres/data \
+-v /mnt/media/postgres/data/:/var/lib/postgresql/data \
+postgres:latest
+
+
+<< postgres14 
+docker run -d \
+--name postgres \
+-p 5432:5432  \
+--restart unless-stopped \
+--user postgres \
+-e POSTGRES_USER=postgres \
+-e POSTGRES_PASSWORD=postgres \
+-v pgdata:/mnt/media/postgres/data/
+-v /mnt/media/postgres/data/pgdata:/var/lib/postgresql/data \
+postgres:latest
+
+docker run -d \
+--name postgres \
+-p 5432:5432  \
+--restart unless-stopped \
+-e POSTGRES_USER=postgres \
+-e POSTGRES_PASSWORD=postgres \
+-e PGDATA=/mnt/media/postgres/data \
 -v /mnt/media/postgres/data:/var/lib/postgresql/data \
 postgres:latest
 
 
+postgres14
 
+
+
+<< pgadmin
 # Postgres - To Login:
 # docker exec -it postgres bash
 # psql -U postgres
@@ -22,3 +59,12 @@ postgres:latest
 #    -e "PGADMIN_DEFAULT_EMAIL=deleonquintero@gmail.com" \
 #    -e "PGADMIN_DEFAULT_PASSWORD=postgres" \
 #    -d dpage/pgadmin4
+pgadmin
+
+
+
+
+
+
+
+

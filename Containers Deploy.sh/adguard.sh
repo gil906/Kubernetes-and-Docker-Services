93eaac9ac1bbbec6639460778d8d8fed89455ce8
Gilberto De Leon <deleonquintero@gmail.com>
2021-11-11 19:55:58 -0500

    Update adguard.sh

--- Containers Deploy.sh/adguard.sh
+++ Containers Deploy.sh/adguard.sh
@@ -14,6 +14,30 @@ docker run -d \
 -p 3000:3000/tcp  \
 adguard/adguardhome
 
+
+
+<<with custom ports
+version: '3.3'
+services:
+    adguardhome:
+        container_name: adguardhome
+        restart: unless-stopped
+        volumes:
+            - '/mnt/media/adguardhome:/opt/adguardhome/work'
+            - '/mnt/media/adguardhome:/opt/adguardhome/conf'
+        ports:
+            - '53:53/tcp'
+            - '53:53/udp'
+            - '67:67/udp'
+            - '67:67/tcp'
+            - '8082:80/tcp'
+            - '4443:443/tcp'
+            - '853:853/tcp'
+            - '3001:3000/tcp'
+        image: adguard/adguardhome
+with custo ports
+
+
 << freeup port 53 ubuntu
 
 On Ubuntu to stop listening port 53 - free up port 53:
@@ -30,5 +54,9 @@ DNS=1.1.1.1
 #Cache=no
 DNSStubListener=no
 #ReadEtcHosts=yes
+
+
+sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
 
 freeup port 53 ubuntu

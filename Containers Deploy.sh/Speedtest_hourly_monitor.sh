96531deee143b9c5c05495cb8dcb4eaac352238a
Gilberto De Leon <deleonquintero@gmail.com>
2022-01-23 22:05:23 -0500

    Create Speedtest_hourly_monitor.sh

---
+++ Containers Deploy.sh/Speedtest_hourly_monitor.sh
@@ -0,0 +1,8 @@
+docker run -d \
+      --name=speedtest \
+      -p 8765:80 \
+     -p 8766:443 \
+      -v /mnt/media/speedtest:/config \
+      -e OOKLA_EULA_GDPR=true \
+      --restart unless-stopped \
+      henrywhitaker3/speedtest-tracker:dev-arm

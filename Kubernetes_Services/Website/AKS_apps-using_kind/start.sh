06c2f7f666df569b1f7fd4e504a3e98b5222b0bc
Gilbert <gil906@hotmail.com>
2024-02-11 23:15:41 -0800

    .

---
+++ Kubernetes_Services/Website/AKS_apps-using_kind/start.sh
@@ -0,0 +1,8 @@
+start.sh: Download all the dependencies and create kind cluster -
+# Usage: bash -f ./start.sh
+#        Supported Options -
+#               --kind-cluster-name=<kind Cluster Name> (default INTEGRATION_TEST_CLUSTER)
+#               --kind-version (default v0.7.0)
+#               --kubectl-version (default v1.18.0)
+#               --helm-version (default v3.2.0)
+#

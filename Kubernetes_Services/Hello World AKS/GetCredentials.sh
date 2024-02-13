faf5218e2a7c17223a5a809d8515d4d199073193
Gilberto De Leon <gdleonq@gmail.com>
2024-02-12 23:34:02 -0800

    Update GetCredentials.sh

--- Kubernetes_Services/Hello World AKS/GetCredentials.sh
+++ Kubernetes_Services/Hello World AKS/GetCredentials.sh
@@ -1,6 +1,7 @@
 # Get your cluster credentials for kubecutl
 # To add Environmental variables on Windows (Powershell): $Env:<variable-name> = "<new-value>"
 # To add Environmental variables on Linux (sudo nano /etc/environment): VARIABLE_NAME="variable_value"
-az aks get-credentials --resource-group $YOURRESOURCEGROUP --n
+az aks get-credentials --resource-group $YOURRESOURCEGROUP --n $CLUSTERNAME
+
 
 
